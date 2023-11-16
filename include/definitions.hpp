3e30b8b0e4ab0b3a070caf89669bee2b2064489b
InterestingError0 <63620497+InterestingError0@users.noreply.github.com>
2023-11-16 15:07:29 -0500

    Add PID and chassis

--- include/definitions.hpp
+++ include/definitions.hpp
@@ -16,6 +16,49 @@ Motor rightC(20, MOTOR_GEAR_BLUE);
 MotorGroup leftMotors({leftA, leftB, leftC});
 MotorGroup rightMotors({rightA, rightB, rightC});
 
+lemlib::Drivetrain_t drivetrain {
+    &leftMotors,
+    &rightMotors,
+    13,
+    3.25,
+    600
+};
+
+Imu inertial(6);
+
+lemlib::OdomSensors_t sensors {
+    nullptr,
+    nullptr,
+    nullptr, 
+    nullptr, 
+    &inertial
+};
+ 
+// forward/backward PID
+lemlib::ChassisController_t lateralController {
+    8, // kP
+    30, // kD
+    1, // smallErrorRange
+    100, // smallErrorTimeout
+    3, // largeErrorRange
+    500, // largeErrorTimeout
+    5 // slew rate
+};
+ 
+// turning PID
+lemlib::ChassisController_t angularController {
+    4, // kP
+    40, // kD
+    1, // smallErrorRange
+    100, // smallErrorTimeout
+    3, // largeErrorRange
+    500, // largeErrorTimeout
+    40 // slew rate
+};
+ 
+// create the chassis
+lemlib::Chassis chassis(drivetrain, lateralController, angularController, sensors);
+
 Motor intake(6, MOTOR_GEAR_BLUE, true, MOTOR_ENCODER_DEGREES);
 
 Motor cata(12,  MOTOR_GEAR_RED, false,  MOTOR_ENCODER_DEGREES);
